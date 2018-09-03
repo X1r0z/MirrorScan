b877d1f0234f4345aa1d73d67699f3220248d325
X1r0z <admin@exp10it.cn>
2018-09-03 21:40:10 +0800

    update database settings

--- wsgi.py
+++ wsgi.py
@@ -3,14 +3,9 @@
 import leancloud
 import os
 
-try:
-    APP_ID = os.environ['LEANCLOUD_APP_ID']
-    APP_KEY = os.environ['LEANCLOUD_APP_KEY']
-    MASTER_KEY = os.environ['LEANCLOUD_APP_MASTER_KEY']
-except KeyError:
-    APP_ID = '9RLabat5zEzNkYtwQW1Rn5sK-gzGzoHsz'
-    APP_KEY = 'M38DE3wMfrPA4HHeRaUkyoIR'
-    MASTER_KEY = 'EFNMBnGv5KazPMgzTNokpj6e'
+APP_ID = '9RLabat5zEzNkYtwQW1Rn5sK-gzGzoHsz'
+APP_KEY = 'M38DE3wMfrPA4HHeRaUkyoIR'
+MASTER_KEY = 'EFNMBnGv5KazPMgzTNokpj6e'
 
 leancloud.init(APP_ID, app_key=APP_KEY, master_key=MASTER_KEY)
 leancloud.use_master_key(False)
